902a73061fef95d9afaab5a07fe72bec2c85a140
rmtar <picpoint@yandex.ru>
2019-09-23 00:06:42 +0300

    work to data

--- dist/js/readDescribe.js
+++ dist/js/readDescribe.js
@@ -2,26 +2,39 @@ const header = document.querySelector('.secondscreen__develophdr');
 const descr = document.querySelector('.secondscreen__developdescr');
 const devlist = document.querySelector('.secondscreen__devlist');
 const devUl = devlist.firstElementChild.childNodes;
-let jsonData = '';
+const mobapp = document.querySelector('#mobapp');
+const webapp = document.querySelector('#webapp');
+const siteintegration = document.querySelector('#siteintegration');
 
-console.log(devUl);
 
 
 function readDataJson() {
+  let str = '';
   const xhr = new XMLHttpRequest();  
   const url = 'https://raw.githubusercontent.com/picpoint/purpleplain/master/dist/data.json';
   xhr.open('GET', url);
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   xhr.addEventListener('readystatechange', () => {
     if(xhr.readyState == 4 && xhr.status == 200) {
-      jsonData = JSON.parse(xhr.responseText);
+      str = JSON.parse(xhr.responseText);
+      
+      console.log(typeof str);
+      console.log(str);
 
-      console.log(jsonData);
+      
+      // for(let key in jsonData) {
+      //   console.log(jsonData[key]);
+      // }
 
     }
   })
   xhr.send();
 }
 
-readDataJson();
+
+
+mobapp.addEventListener('click', function() {
+  readDataJson();
+
+});
 
