463551a8b7ba474bdf3f27c934fc5335b7e285ef
picpoint <picpoint@yandex.ru>
2019-09-24 16:44:40 +0300

    work with adaptive this page

--- gulpfile.js
+++ gulpfile.js
@@ -16,44 +16,47 @@ const settings = {
 
 
 const smartGrigConf = {
-		outputStyle: 'less',
-		colums: 12,
-		offset: '30px',   // межклоночник
-		mobileFirst: false,
-		container: {
-				maxWidth: '1280px',
-				fields: '30px'   // отступ от края экрана
-		},
-		breakPoints: {
-      mgxl: {
-        width: '1800px',
-        fields: '30px'
-      },
-      xxl: {
-        width: '1400px',
-        fields: '30px'
-      },
-			xl: {
-				width: '1140px',
-				fields: '30px'
-			},
-			lg: {
-				width: '960px',
-				fields: '30px'
-			},
-			md: {
-				width: '720px',
-				fields: '20px'
-			},
-			sm: {
-				width: '540px',
-				fields: '10px'
-			},
-			xs: {
-				width: '480px',
-				fields: '5px'
-			}
-		}
+  outputStyle: 'less',
+  colums: 12,
+  offset: '30px',   // межклоночник
+  mobileFirst: false,
+  
+  container: {
+    maxWidth: '1280px',
+    fields: '30px'   // отступ от края экрана
+	},
+  
+  breakPoints: {
+    mgxl: {
+      width: '1800px',
+      fields: '30px'
+    },
+    xxl: {
+      width: '1400px',
+      fields: '30px'
+    },
+    xl: {
+      width: '1140px',
+      fields: '30px'
+    },
+    lg: {
+      width: '960px',
+      fields: '30px'
+    },
+    md: {
+      width: '720px',
+      fields: '20px'
+    },
+    sm: {
+      width: '540px',
+      fields: '10px'
+    },
+    xs: {
+      width: '480px',
+      fields: '5px'
+    }
+  }
+  
 };
 
 gulp.task('preproc', function () {
